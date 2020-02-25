0aefd9eb4686f38ef7cb24634af0f241c1803469
MaeK1R <56087293+MaeK1R@users.noreply.github.com>
2020-02-25 14:18:20 -0600

    Nav mesh location switching working

--- MonochromeMysteries/Assets/Scripts/NavPerson.cs
+++ MonochromeMysteries/Assets/Scripts/NavPerson.cs
@@ -6,13 +6,18 @@ using UnityEngine.AI;
 public class NavPerson : MonoBehaviour
 {
 
-    public Transform targetLocation;
+    public Transform[] targetLocations;
     private GameObject player;
     private bool move;
     private NavMeshAgent agent;
+    private Transform targetLocation;
+    public float waitTime;
+    private float dist;
+    private bool setting;
 
     void Start()
     {
+        SetLocation();
         move = true;
         player = GameObject.FindGameObjectWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
@@ -25,20 +30,47 @@ public class NavPerson : MonoBehaviour
         if (player.activeInHierarchy)
         {
             move = true;
-            MoveToRoom();
+            Move();
         }
         else
         {
             move = false;
         }
+
+        dist = Vector3.Distance(this.transform.position, targetLocation.position);
+        //Debug.Log("Dist: " + dist);
+        if (dist < 2)  
+        {
+            if (setting == false)
+            {
+                SetLocation();
+
+                setting = true;
+            }
+                
+            
+        }
     }
 
-    void MoveToRoom()
+    void Move()
     {
         if (move == true)
         {
             agent.destination = targetLocation.position;
         }
+    }
+    void SetLocation()
+    {
+        int temp = Random.Range(0, targetLocations.Length);
+
+        targetLocation = targetLocations[temp];
+        Move();
+        Invoke("Setting", waitTime);
+        //Debug.Log(setting);
+    }
+    void Setting()
+    {
+        setting = false;
     }
 }
 
