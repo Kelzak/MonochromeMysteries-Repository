8465b6c638c460c1477b094c87ac50eeabda7887
MaeK1R <56087293+MaeK1R@users.noreply.github.com>
2020-02-26 16:18:13 -0600

    fixing, adding, and tuning ai, fixed glowing outlines
    glowing outlines work, added in wander functionaility of AI, added examples in scene

--- MonochromeMysteries/Assets/Scripts/Key Items/Item.cs
+++ MonochromeMysteries/Assets/Scripts/Key Items/Item.cs
@@ -11,8 +11,13 @@ public class Item : MonoBehaviour, IObserver
 {
     public StateChecker stateChecker;
     GameObject item;
+    private float dist;
+    public float playerDistGlow;
+    private GameObject player;
+
 
     private bool isGhost;
+    private bool isClose;
 
     //gets data sent fron statechecker
     public void UpdateData(bool check)
@@ -23,6 +28,10 @@ public class Item : MonoBehaviour, IObserver
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        playerDistGlow = 20f;
+
         item = this.gameObject;
 
         // adds this object to observer list in statechecker
@@ -33,11 +42,26 @@ public class Item : MonoBehaviour, IObserver
     void Update()
     {
         //changes materials 
-        if(isGhost == true)
+        if(isGhost == true && isClose == true)
         {
             item.GetComponent<Outline>().enabled = true;
         }
         else
             item.GetComponent<Outline>().enabled = false;
+
+
+        dist = Vector3.Distance(this.transform.position, player.transform.position);
+        //Debug.Log("Dist: " + dist);
+        if (dist < playerDistGlow)
+        {
+            isClose = true;
+
         }
+        else
+        {
+            isClose = false;
+        }
+    }
 }
+
+
--- MonochromeMysteries/Assets/Scripts/NavPerson.cs
+++ MonochromeMysteries/Assets/Scripts/NavPerson.cs
@@ -12,46 +12,65 @@ public class NavPerson : MonoBehaviour
 
     public Transform[] targetLocations;
     private GameObject player;
-    private bool move;
+    //used to help determine movement
+    private bool move = true;
     private NavMeshAgent agent;
     private Transform targetLocation;
-    public float waitTime;
+
+    public float waitTime = 10f;
+    //if checked the character will use random wait time inbetween the numbers of min and max
+    public bool randWaitTime;
+    private float randWaitTimeMin;
+    private float randWaitTimeMax;
+
+    //used for distance from player calculations
     private float dist;
-    private bool setting;
+    //if checked the person will follow the array consecutively instead of randomly 
     public bool isPath;
-    private int count;
-    public float turnSpeed;
-    //distance until 
-    public float playerDistStop;
+    //if checked character will go to random path
+    public bool wander;
+    public float wanderRadius = 10f;
+    //int used for pathing
+    private int count = 0;
+    //speed person turns to look at player when stopped
+    public float turnSpeed = 3f;
+    //distance until person stops when player gets close
+    public float playerDistStop = 5f;
+
 
     void Start()
     {
-        turnSpeed = 3f;
-        move = true;
         player = GameObject.FindGameObjectWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
-        count = 0;
         agent.isStopped = true;
-        playerDistStop = 4f;
+
+        //calculates the random times based off wait time
+        randWaitTimeMin = waitTime / 2;
+        randWaitTimeMax = waitTime * 2;
     }
 
     [System.Obsolete]
     private void Update()
     {
-        Debug.Log(agent.isStopped);
+        //if stopped, invoke movement over wait time
+        //Debug.Log(agent.isStopped);
         if (agent.isStopped && move == true)
         {
-            Debug.Log("move");
+            if(randWaitTime == true)
+            {
+                waitTime = Random.Range(randWaitTimeMin, randWaitTimeMax);
+            }
+            //Debug.Log("move");
             move = false;
             Invoke("Move", waitTime);
         }
 
+        //if player comes close to character, character stops
         dist = Vector3.Distance(this.transform.position, player.transform.position);
         //Debug.Log("Dist: " + dist);
         if (dist < playerDistStop)
         {
             agent.Stop();
-            
         }
         else
         {
@@ -61,34 +80,47 @@ public class NavPerson : MonoBehaviour
         {
             agent.isStopped = true;
         }
+        //makes character looks towards player
         lookTowards();
 
     }
 
     void Move()
-    {
-
+    { 
         if (isPath == true)
         {
-            count += 1;
-            if (count > targetLocations.Length)
+            
+            if (count >= targetLocations.Length)
             {
                 count = 0;
             }
             targetLocation = targetLocations[count];
+            agent.destination = targetLocation.position;
+            count += 1;
+        }
+        else if(wander == true)
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
+            randomDirection += transform.position;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, wanderRadius, 1);
+            Vector3 finalPosition = hit.position;
+            agent.destination = finalPosition;
+            
         }
         else
         {
             int temp = Random.Range(0, targetLocations.Length);
             targetLocation = targetLocations[temp];
+            agent.destination = targetLocation.position;
         }
-        agent.destination = targetLocation.position;
         agent.isStopped = false;
         move = true;
     }
 
     protected void lookTowards()
     {
+        //look at player only if stopped moving
         if(agent.isStopped == true)
         {
             Vector3 playerPos = player.transform.position;
