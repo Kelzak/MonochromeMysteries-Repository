c28d7b8d4cd73c1474c4c084ce468fca7079ea55
MaeK1R <56087293+MaeK1R@users.noreply.github.com>
2020-04-05 22:44:32 -0500

    new scene!
    some bugs, workin on em. Still need level improvements and textures to advance

---
+++ MonochromeMysteries/Assets/Scripts/FlickerLight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    private Light light;
+    public float minWaitTime;
+    public float maxWaitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            light.enabled = !light.enabled;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
