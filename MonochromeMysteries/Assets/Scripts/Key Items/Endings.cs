1c313036b3ae89dc99913b9f83b7e46d605ca34e
jbove94 <jbove@mail.bradley.edu>
2020-04-12 14:59:16 -0500

    Revert "revert one"
    This reverts commit c5d00cdd960c1464743364f3986d735b0bf44170.

--- MonochromeMysteries/Assets/Scripts/Key Items/Endings.cs
+++ MonochromeMysteries/Assets/Scripts/Key Items/Endings.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class Endings : MonoBehaviour
 {
+    //public Player player;
     public Photographer photographer;
     //public Player player;
     public GameObject knifeInstructions;
@@ -32,6 +33,18 @@ public class Endings : MonoBehaviour
         }
     }
 
+    public IEnumerator KnifeScript()
+    {
+        yield return new WaitForSeconds(2f);
+        Dialogue.AddLine(Dialogue.Character.Pete, "Well I’ll be, if I had to live in this room I’d be madder than a wet hen!And the stench: YUGH!");
+        Dialogue.AddLine(Dialogue.Character.Pete, "Anyhoo, it’s good to see you here, son. It shows me how motivated you are to find who did you wrong, even if it means stepping into other people’s business…and rat shit.");
+        Dialogue.AddLine(Dialogue.Character.Pete, "You remind me a lot of myself back in my hotel inspectin’ days. Hehe, yeah those were crazy times...I reckon that’s why my gut was screamin’ at me to come lend you a helping hand.");
+        Dialogue.AddLine(Dialogue.Character.Pete, "See, without any closure on your life, you’re gonna end up wandering around in this purgatory hell forever, and believe me that’s just enough time to drive any fella mad.");
+        Dialogue.AddLine(Dialogue.Character.Pete, "There’s a lot of things that prohibit you from getting closure, and one of ‘ems your own emotions. I reckon you ain’t too happy about gettin’ whacked, huh? It just ain’t fair...and if you ask this old geezer, ain’t nothing wrong with a little vigilante justice.");
+        Dialogue.AddLine(Dialogue.Character.Pete, "I brought you somethin’ that’ll help you out in that safe over yonder. You’ll be able to use it the way you are now, since I made this safe myself. I hope it finds you your ticket out, and more importantly, a little peace.");
+        Dialogue.AddLine(Dialogue.Character.Pete, "I’ll let you get back to getting revenge on the bastard who put you in this mess. Bless your heart, son.");
+    }
+
     public void UseKnife(string decision)
     {
         if(decision == "kill")
@@ -39,12 +52,12 @@ public class Endings : MonoBehaviour
             if(photographer.GetComponent<Player>()) //you win
             {
                 Debug.Log("You chose correctly!");
-                //SceneManager.LoadScene("WinScene");
+                SceneManager.LoadScene("WinScene");
             }
             else if(photographer.GetComponent<Player>() == null) //you lose
             {
                 Debug.Log("You chose wrong :(");
-                //SceneManager.LoadScene("LoseScene");
+                SceneManager.LoadScene("LoseScene");
             }
         }
         else if(decision == "notyet")
