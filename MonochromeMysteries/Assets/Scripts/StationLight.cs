53c84cfe43e2fc81a8cbad1468d032108a8407d2
MaeK1R <56087293+MaeK1R@users.noreply.github.com>
2021-02-19 03:30:01 -0600

    station light

---
+++ MonochromeMysteries/Assets/Scripts/StationLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationLight : MonoBehaviour
+{
+    [SerializeField]
+    private Light light;
+
+    private bool stationPower = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = this.GetComponent<Light>();
+        light.enabled = stationPower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PowerSwitch.stationPowerOn)
+        {
+            stationPower = true;
+            ActivateLight();
+        }
+    }
+
+    void ActivateLight()
+    {
+        light.enabled = true;
+    }
+}
