1f0a97205816ff3167c25a09f760245d495f70d1
Kevon Long <krlong@mail.bradley.edu>
2021-03-03 21:22:41 -0600

    Suitcase Puzzle almost done, new clues are in

--- MonochromeMysteries/Assets/Scripts/Suitcase.cs
+++ MonochromeMysteries/Assets/Scripts/Suitcase.cs
@@ -1,36 +1,147 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Suitcase : ItemAbs
 {
     public int slot1;
+    public TMP_Text slot1Text;
     public int slot2;
+    public TMP_Text slot2Text;
     public int slot3;
-    private static string correctOrder = "369";
-    private static string inputtedCode = "";
+    public TMP_Text slot3Text;
+    private static int correctNum1 = 3;
+    private static int correctNum2 = 6;
+    private static int correctNum3 = 9;
+    //private static string inputtedCode = "";
 
+    public bool puzzleActivated;
+
+    public Button number1Up;
+    public Button number1Down;
+    public Button number2Up;
+    public Button number2Down;
+    public Button number3Up;
+    public Button number3Down;
+
+    public GameObject suitcaseUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        puzzleActivated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slot1Text.text = slot1.ToString();
+        slot2Text.text = slot2.ToString();
+        slot3Text.text = slot3.ToString();
+
+        if (puzzleActivated)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                HidePadlock();
+            }
+            
+            if(Input.GetKeyDown(KeyCode.F2))
+            {
+                CheckIfCodeisCorrect();
+            }
+        }
+    }
+
+    public void NumberUp(string buttonName)
+    {
+        if (buttonName.Contains("1"))
+        {
+            if (slot1 != 9)
+                slot1 += 1;
+        }
+        if (buttonName.Contains("2"))
+        {
+            if (slot2 != 9)
+                slot2 += 1;
+        }
+        if (buttonName.Contains("3"))
+        {
+            if (slot3 != 9)
+                slot3 += 1;
+        }
+    }
+
+    public void NumberDown(string buttonName)
+    {
+        if (buttonName.Contains("1"))
+        {
+            if(slot1 != 0)
+                slot1 -= 1;
+        }
+        if (buttonName.Contains("2"))
+        {
+            if (slot2 != 0)
+                slot2 -= 1;
+        }
+        if (buttonName.Contains("3"))
+        {
+            if (slot3 != 0)
+                slot3 -= 1;
+        }
+    }
+
+    public void CheckIfCodeisCorrect()
+    {
+        if(correctNum1 == slot1 && correctNum2 == slot2 && correctNum3 == slot3)
+        {
+            Debug.Log("suitcase opened");
+        }
+        else
+        {
+            Debug.Log("Wrong code try again");
+        }
     }
 
     public override void Activate()
     {
-        throw new System.NotImplementedException();
+        ShowPadlock();
+    }
+
+    private void ShowPadlock()
+    {
+        suitcaseUI.SetActive(true);
+        Time.timeScale = 0;
+        Player.canMove = false;
+        Player.canLook = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        puzzleActivated = true;
+    }
+
+    private void HidePadlock()
+    {
+        suitcaseUI.SetActive(false);
+        Time.timeScale = 1;
+        Player.canMove = true;
+        Player.canLook = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        puzzleActivated = false;
     }
 
     public override void SetItemUI()
     {
-        throw new System.NotImplementedException();
+        if (StateChecker.isGhost)
+        {
+            GetComponent<Item>().SetUI(null, null, null, "Spirit can't use", true);
+        }
+        else
+        {
+            GetComponent<Item>().SetUI(null, null, null, "Press F to unlock suitcase", true);
+        }
     }
 }
